49883775d4083418e9b25d22b784fb64da478fbb
Hardyx07 <hardik.tailor@kalvium.community>
2025-01-29 11:08:48 +0530

    milestone8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import {LoginPage} from './Routes.jsx'
 import { SignupPage } from "./Routes.jsx";
+import {Home } from "./Routes.jsx";
 
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
