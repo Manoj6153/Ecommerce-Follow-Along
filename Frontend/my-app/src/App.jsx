789c9a666ccb976c6976b8f8ed5399f662931c3e
Manoj <manojb6153@gmail.com>
2025-02-04 11:35:10 +0530

    added product form

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Login from "./component/Login"
 import Sign from "./component/Signup"
 import {Home} from "./pages/Home"
+import {Productform} from "./component/Productform"
 import './App.css'
 import {Routes,Route} from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Sign/>}/>
+        <Route path="/productform" element={<Productform/>}/>
       </Routes>
 
     </>
