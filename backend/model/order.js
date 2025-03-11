5685d9d3003582cf79b71526f46771b7b7373450
Manoj <manojb6153@gmail.com>
2025-03-11 11:55:26 +0530

    done

--- backend/model/order.js
+++ backend/model/order.js
@@ -1,12 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const orderSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    quantity: { type: Number, required: true },
-    address: { type: Object, required: true },
-    status: { type: String, default: 'Pending' },
-    createdAt: { type: Date, default: Date.now }
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true
+    },
+    productId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Products',
+        required: true 
+    },
+    quantity: { 
+        type: Number, 
+        required: true 
+    },
+    address: { 
+        type: Object, 
+        required: true 
+    },
+    status: { 
+        type: String, 
+        default: 'Pending' 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
